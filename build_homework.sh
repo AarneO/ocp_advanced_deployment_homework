b7d4701a760b90170cc9c0a288413554c40986a0
Aarne Ojanpera <aarne@redhat.com>
2018-08-18 22:51:45 +0300

    build script

---
+++ build_homework.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+export GUID=`hostname|awk -F. '{print $2}'`
+
+echo "This will build the Openshift Advanced Deployment Homework Assignment"
+echo ""
+echo "GUID for the environment seems to be $GUID, bulding the inventory file with that"
+echo ""
+echo ansible-playbook -e "guid=$GUID" --tags "config"
+echo "Starting OpenShift deployment"
+echo ansible-playbook -e "guid=$GUID" --skip-tags "config"
+echo ""
+smoketest=$(oc get route |grep nodejs-mongo | awk '{print $2}')
+echo "Smoke test URL can be found http://$smoketest"
+echo ""
