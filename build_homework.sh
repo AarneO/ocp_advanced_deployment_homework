e19f9567549848086b3e7f30056bd806b524e4c3
Aarne Ojanpera <aarne@redhat.com>
2018-08-18 22:53:03 +0300

    build script

--- build_homework.sh
+++ build_homework.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 export GUID=`hostname|awk -F. '{print $2}'`
 
+echo ""
 echo "This will build the Openshift Advanced Deployment Homework Assignment"
 echo ""
 echo "GUID for the environment seems to be $GUID, bulding the inventory file with that"
