eeeaf632f750615d95b111d7ffe7045be175db27
Aarne Ojanpera <aarne@redhat.com>
2018-08-28 13:44:27 +0300

    label fix

--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -24,6 +24,10 @@ htpasswd -b /root/htpasswd.openshift.fixed $1 r3dh4t1!
 echo "Syncing file to all masters"
 ansible masters -m copy -a "src=/root/htpasswd.openshift.fixed dest=/etc/origin/master/htpasswd"
 
+echo "verify login"
+oc login https://loadbalancer1.$GUID.internal:8443 --username=$1 --password=r3dh4t1! --insecure-skip-tls-verify=false
+oc login -u system:admin
+
 echo "add correct environment label to user"
 oc label user/$1 client=$2
 
