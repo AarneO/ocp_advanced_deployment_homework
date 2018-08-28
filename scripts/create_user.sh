405cd14c35ee21002d9cd4780b57f3c9226bed2c
Aarne Ojanpera <aarne@redhat.com>
2018-08-28 14:35:01 +0300

    user script mod

--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -27,13 +27,13 @@ echo "Adding user $1 to htpasswd file"
 htpasswd -b /root/htpasswd.openshift.fixed $1 r3dh4t1!
 
 echo "Syncing file to all masters"
-ansible masters -m copy -a "src=/root/htpasswd.openshift.fixed dest=/etc/origin/master/htpasswd"
+ansible masters -m copy -a "src=/root/htpasswd.openshift.fixed dest=/etc/origin/master/htpasswd" >> /dev/null
 
 echo "verify login"
 oc login https://loadbalancer1.$GUID.internal:8443 --username=$1 --password=r3dh4t1! --insecure-skip-tls-verify=false >> /dev/null
 oc login -u system:admin >> /dev/null
 
 echo "add correct environment label to user"
-oc label user/$1 client=$2
+oc label user/$1 client=$2 --overwrite
 
 echo "User created and labeled, password set to environment default"
